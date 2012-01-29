cfa4799deaac5ea146eb23ddfd78d3dff624d44a
Paul Sbarra <Sbarra.Paul@gmail.com>
2012-01-29 15:42:35 -0600

    Add copyright / license information

--- cl/opencl_test.go
+++ cl/opencl_test.go
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2012 Paul Sbarra
+ *
+ * This file is part of go-opencl.
+ *
+ * go-opencl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * go-opencl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with go-opencl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package cl
 
 import (
